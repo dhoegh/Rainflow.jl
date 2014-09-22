d8196b042e63ef89d774c13c36dafd2f90ef0659
Daniel Høegh <dhoegh91@gmail.com>
2014-09-22 19:48:57 +0200

    Added testsuite to the package.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using Rainflow
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+srand(1234)
+signal = randn(100)
+ext, t = sort_peaks(signal)
+result = find_cycles(ext,t)
+bins = calc_sum(result,13,1)
+@test squeeze(bins,2) == [4.0, 6.0, 7.5, 3.0, 2.5, 5.0, 0.0, 2.5, 1.0, 0.0, 0.5, 0.0, 1.0]
