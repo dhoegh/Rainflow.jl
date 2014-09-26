026672f1d03f0d11bd809b3b66b401bbb0bf8eff
Daniel Høegh <dhoegh91@gmail.com>
2014-09-26 15:27:23 +0200

    Added show methods and Cycle is now immutable.

--- src/Rainflow.jl
+++ src/Rainflow.jl
@@ -1,19 +1,17 @@
 module Rainflow
+import Base.show
 
 export sort_peaks, check_max, find_cycles, calc_sum
 
-
-
-
+""" This function sort out points where the slope is changing sign."""
 function sort_peaks(signal::AbstractArray{Float64,1}, dt=[0.:length(signal)-1.])
-    """ This sort out points where the slope is changing sign"""
     slope = diff(signal)
     # Determines if the point is local extremum
-    is_extremum = [true ;(slope[1:end-1].*slope[2:end]).<=0.;true] 
+    is_extremum = [true ;(slope[1:end-1].*slope[2:end]).<=0.;true]
     return signal[is_extremum] , dt[is_extremum]
 end
 
-type Cycle  # This is the information stored for each cycle found
+immutable Cycle  # This is the information stored for each cycle found
     count::Float64
     range::Float64
     mean::Float64  # value
@@ -23,13 +21,15 @@ type Cycle  # This is the information stored for each cycle found
     t_e::Float64   # time end
 end
 
+show(io::IO,x::Cycle) = print(io, "Cycle: count=", x.count, ", range=",x.range, ", mean=",x.mean)
+
 function cycle(count::Float64, v_s::Float64, t_s::Float64, v_e::Float64, t_e::Float64)
     Cycle(count, abs(v_s-v_e), (v_s+v_e)/2, v_s, t_s, v_e, t_e)
 end
 
 function find_cycles(ext_in::Array{Float64,1},t::Array{Float64,1})
     ext = copy(ext_in) # Makes a copy because there is going to be sorted in the vectors
-    time = copy(t) 
+    time = copy(t)
     i = 1
     j = 2
     cycles = Cycle[]
@@ -39,12 +39,12 @@ function find_cycles(ext_in::Array{Float64,1},t::Array{Float64,1})
         Y = abs(ext[i+1]-ext[i])
         X = abs(ext[j+1]-ext[j])
         if X>=Y
-            if i == 1 # This case covers a half cycle deletes the poit that is counted
+            if i == 1 # This case counts a half and cycle deletes the poit that is counted
                 #println("Half cycle $(ext[i]), $(ext[i+1])")
                 push!(cycles,cycle(0.5 ,ext[i], time[i], ext[i+1],time[i+1]))
                 shift!(ext) # Removes the first entrance in ext and time
                 shift!(time)
-            else # This case covers a one cycle deletes the poit that is counted
+            else # This case counts one cycle and deletes the poit that is counted
                 #println("One cycle $(ext[i]), $(ext[i+1])")
                 push!(cycles,cycle(1. ,ext[i], time[i], ext[i+1],time[i+1]))
                 splice!(ext,i+1)  # Removes the i and i+1 entrance in ext and time
@@ -63,18 +63,20 @@ function find_cycles(ext_in::Array{Float64,1},t::Array{Float64,1})
         #println("Half cycle $(ext[i]), $(ext[i+1])")
         push!(cycles,cycle(0.5 ,ext[i], time[i], ext[i+1],time[i+1]))
     end
-    return cycles
     end
+    return cycles
 end
 
-type cycle_stats # 
+type Cycle_stats #
     min_mean::Float64
     max_mean::Float64
     max_range::Float64
 end
 
+show(io::IO,x::Cycle_stats) = print(io, "Cycle stats: min mean value=", x.min_mean, ", max mean value=", x.max_mean, ", max range=",x.max_range)
+
 function check_max(cycles::Array{Cycle,1})
-    stats = cycle_stats(Inf, -Inf, -Inf)
+    stats = Cycle_stats(Inf, -Inf, -Inf)
     for cycle in cycles
         cycle.mean > stats.max_mean && setfield!(stats, :max_mean, cycle.mean)
         cycle.mean < stats.min_mean && setfield!(stats, :min_mean, cycle.mean)
@@ -99,9 +101,9 @@ function calc_sum{T<:Real}(cycles::Array{Cycle,1}, range_spacing::Array{T,1}, me
     #show(range_spacing)
     mean_spacing *= (stats.max_mean-stats.min_mean)/100
     mean_spacing += stats.min_mean
-	nr_digits = 14  # The rounding is performed due to numerical noise in the floats when comparing
-	mean_spacing = round(mean_spacing, nr_digits)
-	range_spacing = round(range_spacing, nr_digits)
+    nr_digits = 14  # The rounding is performed due to numerical noise in the floats when comparing
+    mean_spacing = round(mean_spacing, nr_digits)
+    range_spacing = round(range_spacing, nr_digits)
     #show(mean_spacing)
     for cycle in cycles
         i = find_range(range_spacing,round(cycle.range, nr_digits))
@@ -111,16 +113,16 @@ function calc_sum{T<:Real}(cycles::Array{Cycle,1}, range_spacing::Array{T,1}, me
     return bins
 end
 
-function calc_sum(cycles::Array{Cycle,1}, nr_ranges::Int=13, nr_means::Int=1)
+function calc_sum(cycles::Array{Cycle,1}, nr_ranges::Int=10, nr_means::Int=1)
     range_spacing = linspace(0,100,nr_ranges+1)
     mean_spacing = linspace(0,100,nr_means+1)
     calc_sum(cycles, range_spacing, mean_spacing)
 end
 
 try
-	include("plot.jl")
+    include("plot.jl")
 catch
-	println("""For added plotting features do: Pkg.add("PyPlot")""")
+    println("""For added plotting features do: Pkg.add("PyPlot")""")
 end
 
 end # module
